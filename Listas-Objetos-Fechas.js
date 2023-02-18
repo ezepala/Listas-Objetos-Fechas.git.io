1eb6f6f052ff5a8e10125e6ba9b5a42841ae7d73
ezepala <122852104+ezepala@users.noreply.github.com>
2023-02-18 00:42:47 -0300

    Add files via upload

---
+++ Listas-Objetos-Fechas.js
@@ -0,0 +1,14 @@
+const lista = ["David, 31, true, true"]
+console.log(lista)
+
+const libro = {
+    nombre: "El señor de los Anillos",
+    paginas: 4000,
+    autor: "JR Tolkien"
+    
+
+}
+console.log(libro)
+
+const fecha = new Date(1991, 7, 9)
+console.log(fecha)
